3fc4e6fc71621229b09a6fe6f013d88b1f27c646
Francisco Amado <franciscoamado@live.com.pt>
2020-02-01 22:29:08 +0000

    cleanup

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,10 @@
-import FluentSQLite
+//import FluentSQLite
 import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register providers first
-    try services.register(FluentSQLiteProvider())
+//    try services.register(FluentSQLiteProvider())
 
     // Register routes to the router
     let router = EngineRouter.default()
@@ -17,16 +17,13 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
 
-    // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
 
-    // Register the configured SQLite database to the database config.
-    var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
-    services.register(databases)
+//    databases.add(as: , database: .sqlite)
+//    databases.add(database: sqlite, as: .sqlite)
+//    services.register(databases)
 
     // Configure migrations
-    var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
-    services.register(migrations)
+//    var migrations = MigrationConfig()
+//    migrations.add(model: Todo.self, database: .sqlite)
+//    services.register(migrations)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -13,8 +13,8 @@ public func routes(_ router: Router) throws {
     }
 
     // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+//    let todoController = TodoController()
+//    router.get("todos", use: todoController.index)
+//    router.post("todos", use: todoController.create)
+//    router.delete("todos", Todo.parameter, use: todoController.delete)
 }
