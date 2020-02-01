d87a812592cbec06a4b92d5f5d54a0b829170a6e
Francisco Amado <franciscoamado@live.com.pt>
2020-02-01 22:30:44 +0000

    initial crawler

---
+++ Sources/App/Crawler/crawler.swift
@@ -0,0 +1,20 @@
+//
+//  crawler.swift
+//  App
+//
+//  Created by Francisco Amado on 30/11/2019.
+//
+
+import Vapor
+import Foundation
+import NIO
+import MongoSwift
+
+//public func crawl(_ task: RepeatedTask) {
+//    print("crawl \(task)")
+//}
+
+public func crawl(_ app: Application) {
+    print("crawl \(app)")
+
+}
--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -3,4 +3,8 @@ import Vapor
 /// Called after your application has initialized.
 public func boot(_ app: Application) throws {
     // Your code here
+//    app.eventLoop.scheduleRepeatedTask(initialDelay: .seconds(1), delay: .seconds(3), crawl)
+    app.eventLoop.scheduleTask(in: .seconds(1)) {
+        crawl(app)
+    }
 }
